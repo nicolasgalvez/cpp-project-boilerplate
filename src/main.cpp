cbfd167826757d95738b34756fa43fa5dabf595c
Nick Galvez <nicholas.galvez@gmail.com>
2024-10-17 17:35:23 -0700

    autocommit

--- src/main.cpp
+++ src/main.cpp
@@ -8,191 +8,81 @@
  */
 
 #include <iostream>
-#include <algorithm> // for sort(), find()
-#include <numeric>   // for accumulate()
-#include <vector>
-#include <fstream>
-#include <sstream>
-#include <string>
+#include <iomanip>
 
 using namespace std;
+const int SIZE = 3;
 
-/**
- * EnvironmentReading struct
- *
- * @param time_t time
- * @param double temperature
- * @param double soilMoisture
- * @param double battery
- */
-struct EnvironmentReading
-{
-    time_t time;
-    double temperature;
-    double soilMoisture;
-    double battery;
-    ~EnvironmentReading() {}
+class Chair {
+private:
+    int legs;
+    double * prices;
+public:
+    // constructors
+    Chair() {
+        prices = new double[SIZE];
+        legs = 0;
+        for (int i = 0; i < SIZE; i++)
+            prices[i] = 0;
+    }
+    Chair(int l) {
+        prices = new double[SIZE];
+        legs = l;
+        for (int i = 0; i < SIZE; i++)
+            prices[i] = 0;
+    }
+
+    // setters and getters
+    void setLegs(int l)      { legs = l; }
+    int getLegs()            { return legs; }
+
+    void setPrices(double p1, double p2, double p3) { 
+        prices[0] = p1; prices[1] = p2; prices[2] = p3; 
+    }
+
+    double getAveragePrices() {
+        double sum = 0;
+        for (int i = 0; i < SIZE; i++)
+            sum += prices[i];
+        return sum / SIZE;
+    }
+
+    void print() {
+        cout << "CHAIR DATA - legs: " << legs << endl;
+        cout << "Price history: " ;
+        for (int i = 0; i < SIZE; i++)
+            cout << prices[i] << " ";
+        cout << endl << "Historical avg price: " << getAveragePrices();
+        cout << endl << endl;
+    }
 };
 
-/**
- * Prints the time in a human-readable format.
- *
- * @param time_t time
- * @param string format
- * @return string
- */
-string printTime(const time_t &time, const string &format = "%Y-%m-%d %H:%M:%S")
-{
-    char buffer[80];
-    strftime(buffer, 80, format.c_str(), localtime(&time));
-    return buffer;
-}
+int main() {
+    cout << fixed << setprecision(2);
 
-/**
- * Function to read a TSV, based on the restaurant assignment
- *
- * @param string filename
- * @return vector<EnvironmentReading>
- */
-vector<EnvironmentReading> readFromFile(string filename)
-{
-    vector<EnvironmentReading> temp;
-    // check for valid file
-    ifstream file(filename);
-    if (!file)
-    {
-        cout << "File not found." << endl;
-        return temp;
-    }
+    //creating pointer to first chair object
+    Chair *chairPtr = new Chair;
+    chairPtr->setLegs(4);
+    chairPtr->setPrices(121.21, 232.32, 414.14);
+    chairPtr->print();
 
-    string line;
-    int lineCount = 0;
-    // get the header
-    getline(file, line);
-    // Read each line of the file
-    while (getline(file, line))
-    {
+    //creating dynamic chair object with constructor
+    Chair *livingChair = new Chair(3);
+    livingChair->setPrices(525.25, 434.34, 252.52);
+    livingChair->print();
+    delete livingChair;
+    livingChair = nullptr;
 
-        // cout << "Processing line " << lineCount << endl; // Debug
-        EnvironmentReading reading;
-        stringstream ss(line);
-
-        // Parse each field using tab as the delimiter
-        // Note to self: make sure your TSV actually has tabs, and not 4 spaces. Thanks VSCode.
-        string field;
-        getline(ss, field, '\t');
-
-        // I thought this would be quick and easy, but it's been like an hour. I guess I need better "time management" har har.
-        string timeString = field;
-        tm timeStruct = {};
-        istringstream iss(timeString);
-        iss >> get_time(&timeStruct, "%Y-%m-%d %H:%M:%S");
-        // For some reason it's off by an hour. Daylight savings no doubt.
-        timeStruct.tm_isdst = -1; // Set the daylight savings time flag to -1
-        reading.time = mktime(&timeStruct);
-        // cout << "Time: " << printTime(reading.time) << endl; // debug
-
-        getline(ss, field, '\t');
-        reading.temperature = stod(field);
-        // cout << "Temperature: " << reading.temperature << endl; // debug
-
-        getline(ss, field, '\t');
-        reading.soilMoisture = stod(field);
-        // cout << "Soil Moisture: " << reading.soilMoisture << endl; // debug
-
-        getline(ss, field, '\t');
-        reading.battery = stod(field);
-        // cout << "Battery: " << reading.battery << endl; // debug
-
-        // Add the restaurant to the array
-        temp.push_back(reading);
-    }
-    return temp;
-}
-
-int main()
-{
-    // Get a new array of EnvironmentReading structs
-    // Test file should be in the repo under the src directory.
-    vector<EnvironmentReading> readings = readFromFile("../src/test.tsv");
-
-    // print out the first 5 readings
-    cout << "First 5 readings: " << endl;
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "Time: " << readings[i].time << endl;
-        cout << "Temperature: " << readings[i].temperature << endl;
-        cout << "Soil Moisture: " << readings[i].soilMoisture << endl;
-        cout << "Battery: " << readings[i].battery << endl;
-        cout << endl;
-    }
-    cout << "The array thinks it's this big: " << readings.size() << " but it's actually this big: " << readings.size() << endl;
-    cout << endl;
-    // Get average temperature
-    // We can't use the size() method, because I've come to learn that it's impossible to resize a std::array once it's been allocated.
-    // Confusing since we were able to do that with array pointers... maybe I'm missing something.
-    double sumTemperature = 0;
-    for (int i = 0; i < readings.size(); i++)
-    {
-        sumTemperature += readings[i].temperature;
-    }
-
-    cout << setprecision(3) << "Average temperature: " << sumTemperature / readings.size() << endl;
-
-    // report starting and ending time:
-    cout << "Starting time: " << printTime(readings.front().time) << endl;
-    cout << "Ending time: " << printTime(readings.back().time) << endl;
-    cout << endl;
-    // Seach by temperature
-    vector<EnvironmentReading>::iterator readingsIterator;
-    cout << "Search by temperature: 80" << endl;
-    double target = 80;
-    readingsIterator = find_if(readings.begin(), readings.end(), [target](const EnvironmentReading &reading)
-                               { return reading.temperature == target; });
-
-    // print value of readingsIterator
-    if (readingsIterator != readings.end())
-    {
-        cout << "Temperature " << target << " found in position " << readingsIterator - readings.begin() << endl;
-        cout << "Time: " << printTime(readingsIterator->time) << endl;
-        cout << "Temperature: " << readingsIterator->temperature << "F" << endl;
-        cout << "Soil Moisture: " << readingsIterator->soilMoisture << endl;
-        cout << "Battery: " << readingsIterator->battery << endl;
-        cout << endl;
-    }
-    else
-    {
-        cout << "Temperature " << target << " was not found." << endl;
-    }
-
-    // Get the max temperature
-    readingsIterator = max_element(readings.begin(), readings.end(), [](const EnvironmentReading &a, const EnvironmentReading &b)
-                                   {
-                                       return a.temperature < b.temperature; // Compare temperatures
-                                   });
-    if (readingsIterator != readings.end())
-    {
-        cout << "Max temperature: " << readingsIterator->temperature << "F";
-        cout << " at " << printTime(readingsIterator->time, "%H:%M") << endl;
-    }
-
-    // At what time was the solar battery fully charged?
-    readingsIterator = find_if(readings.begin(), readings.end(), [](const EnvironmentReading &reading)
-                               { return reading.battery >= 1; });
-    if (readingsIterator != readings.end())
-    {
-        cout << "Battery fully charged at: " << printTime(readingsIterator->time) << endl;
-    }
-
-    // Use accumulate to get the total number of hours the battery was fully charged, this is a double because we take measurements every 15 minutes
-    int hoursFullyCharged = accumulate(readings.begin(), readings.end(), 0, [](int sum, const EnvironmentReading &reading)
-                                       { return reading.battery >= 1 ? sum + 1 : sum; });
-    // Try to find the start index and end index of the fully charged battery
-    size_t timeStartIndex = distance(readings.begin(), readingsIterator);
-    time_t endTime = readings.at(timeStartIndex + hoursFullyCharged - 1).time; // I guess we have to subtract 1, otherwise there's an out of bounds exception. 
-    // Get the time difference in seconds and convert to hours
-    double differenceInSeconds = difftime(endTime, readingsIterator->time) / 60 / 60;
-    cout << "End time: " << printTime(endTime) << endl;
-    // This is what I expected vs last night's output.
-    cout << setprecision(3) << "Battery was fully charged for about " << differenceInSeconds << " hours." << endl;
+    //creating dynamic array of chair objects
+    Chair *collection = new Chair[SIZE];
+    collection[0].setLegs(4);
+    collection[0].setPrices(441.41, 552.52, 663.63);
+    collection[1].setLegs(4);
+    collection[1].setPrices(484.84, 959.59, 868.68);
+    collection[2].setLegs(4);
+    collection[2].setPrices(626.26, 515.15, 757.57);
+    for (int i = 0; i < SIZE; i++)
+        collection[i].print();
+    
+    return 0;
 }
