b0320814b5b216452470beda218d897c7c468455
Nick Galvez <nicholas.galvez@gmail.com>
2024-10-17 20:03:51 -0700

    autocommit

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,48 @@
 /**
  * Title: Midterm 2
- * Description: 
+ * Description: Simulate a line at a coffee shop. Your simulation should run for 20 time periods (minutes) and display the current line at the coffee shop.
  * Author: Nick Galvez
  * Class: COMSC-210
  *
  */
+
+// When the store opens (i.e. in first time period), add 5 customers to the line right away.
+
+/**
+In subsequent time periods, the probability of:
+
+A customer being helped at the beginning of the line and ordering their coffee is 40%
+A new customer joining the end of the line is 60%
+The customer at the end of the line deciding they don't want to wait and leaving is 20%
+Any particular customer can decide they don't want to wait and leave the line: 10%
+A VIP (very important person) customer with a Coffee House Gold Card gets to skip the line and go straight to the counter and order: 10%
+
+
+The status reporting should look like:
+
+A customer joins the line:
+
+Jean joined the line
+A customer leaves the line before being served:
+
+Jean left the line
+A VIP customer joins the front of the line:
+
+Jean (VIP) joins the front of the line
+A customer is served:
+
+Jean is served
+The end customer gets frustrated and leaves the line:
+
+Jean (at the rear) left the line
+ */
+
+
 #include <iostream>
+
 using namespace std;
 
-const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
+const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20, TIME_PERIODS = 20;
 
 class DoublyLinkedList {
 private:
@@ -210,9 +244,18 @@ public:
     }
 };
 
+// Process the line
+void timeWarp(){
+    // again
+    DoublyLinkedList line;
+
+}
+
+
+
 int main() {
     cout << MIN_NR + MIN_LS + MAX_NR + MAX_LS;  // dummy statement to avoid compiler warning
-
+    for
     
     return 0;
 }
