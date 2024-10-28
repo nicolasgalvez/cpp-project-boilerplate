0b64da94e13baa8856694a05057d932bb3dd42ff
Nick Galvez <nicholas.galvez@gmail.com>
2024-10-27 22:09:00 -0700

    Add comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,10 @@
 /**
  * Title: Lab 25: Data Structures Races
- * Description: We will code data structures at the races!
+ * Description: I ran out of time on this one. I keep trying to make patterns that use hash arrays, like PHP and loose typing. I want to be able to make an array of the contenders and then loop through them.
  * Author: Nick Galvez
  * Class: COMSC-210
  *
  */
-
-/**
- * The starter code has a data file that is 20,000 lines long and contains random strings representing codes or IDs.
-
-Which data structure is fastest?
-
-We will code data structures at the races! In each race, the contestants (a vector, list, and set) will all compete against each other. There will be four various races! Write a program that will time, record, and report how long it takes each contestant to run these four races:
-
-The first race is READING. The challenge is to read the 20,000 data elements into each of these data structures: vector, list, set
-The second race is SORTING. Sort the vector and sort the list. Set the set's value to -1 since a set is already sorted.
-The third race is INSERTING. Insert the value "TESTCODE" into the middle of the vector or the middle of the list, or into the set
-The fourth race is DELETING. Delete the middle-ish element of the vector, the list, or the set
-Use the cool time features of the <chrono> library. You'll just be using a stopwatch on these operations.
- */
-
 
 #include <list>
 #include <fstream>
@@ -31,38 +16,39 @@ Use the cool time features of the <chrono> library. You'll just be using a stopw
 using namespace std;
 using namespace std::chrono;
 
-
 const int MAX_CODES = 20000;
 // auto<string> contenders[3] = {vector<string>(), list<string>(), set<string>()}; no work.
 // vector<string> vectorContender;
 // list<string> listContender;
 // set<string> setContender;
 
-struct Clock {
+struct Clock
+{
     high_resolution_clock::time_point start;
     high_resolution_clock::time_point end;
     duration<double> duration;
 };
 
-
-struct Contenders {
+struct Contenders
+{
     vector<string> vectorContender;
     list<string> listContender;
     set<string> setContender;
-    ~Contenders() {
+    ~Contenders()
+    {
         vectorContender.clear();
         listContender.clear();
         setContender.clear();
     }
 };
-/** 
+/**
  * Probably a simpler way to do this.
  * This was a fun assignment but I ran out of time!
  * I'll keep working on it though.
- * 
  */
-class Race {
-    public:
+class Race
+{
+public:
     Contenders contenders;
     string codes[MAX_CODES];
     string name;
@@ -71,37 +57,39 @@ class Race {
     high_resolution_clock::time_point end;
     duration<double> duration;
 
-    Race (string name) {
+    Race(string name)
+    {
         this->name = name;
     }
-    Race (string name, string codes[]) {
+    Race(string name, string codes[])
+    {
         this->name = name;
         copy(codes, codes + MAX_CODES, this->codes); // didn't work with just this->codes = codes;
     }
-    void race(string name, function<void(Contenders&, string)> insertFunc) {
-    
-    // start the clock and then run the callback
-    start = high_resolution_clock::now();
+    void race(string name, function<void(Contenders &, string)> insertFunc)
+    {
 
-    for (int i = 0; i < MAX_CODES; i++) {
-        insertFunc(contenders, codes[i]);
-    }
-    // end the clock
-    end = high_resolution_clock::now();
+        // start the clock and then run the callback
+        start = high_resolution_clock::now();
 
+        for (int i = 0; i < MAX_CODES; i++)
+        {
+            insertFunc(contenders, codes[i]);
+        }
+        // end the clock
+        end = high_resolution_clock::now();
     }
 
 }
-
 
 /**
  * The race function takes the contenders, the codes, and a callback (If I can figure that out)
  * https://stackoverflow.com/questions/2298242/callback-functions-in-c
  */
 
-
-
-void displayResults(Clock clock) {
+void
+displayResults(Clock clock)
+{
     /**
      *  Operation    Vector      List       Set
       Read      4081      4664     12682
@@ -112,8 +100,6 @@ void displayResults(Clock clock) {
     cout << "Operation" << setw(11) << "Vector" << setw(11) << "List" << setw(5) << "Set" << endl;
     cout << clock.duration.count() << endl;
 }
-
-
 
 int main()
 {
@@ -127,9 +113,10 @@ int main()
         cout << "Error opening file." << endl;
         return 1;
     }
-    
+
     int i = 0;
-    while (fin >> codes[i++]) ;
+    while (fin >> codes[i++])
+        ;
     cout << "Read " << i << " codes." << endl;
     fin.close();
 
@@ -142,7 +129,8 @@ int main()
 
     // Example loop to measure
     vector<int> numbers;
-    for(int i = 0; i < 1000000; ++i) {
+    for (int i = 0; i < 1000000; ++i)
+    {
         numbers.push_back(i);
     }
 
