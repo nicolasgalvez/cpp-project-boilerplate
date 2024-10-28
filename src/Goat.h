8dd60f178454159f06a256204f03b39194313ea9
Nick Galvez <nicholas.galvez@gmail.com>
2024-10-27 19:52:08 -0700

    autocommit

--- src/Goat.h
+++ src/Goat.h
@@ -5,24 +5,40 @@
 #include <iostream>
 using namespace std;
 
-class Goat {
+class Goat
+{
 private:
     string name;
     int age;
     string color;
-public: 
-    Goat()                          { name = ""; age = 0; color = ""; }
+
+public:
+    Goat()
+    {
+        name = "";
+        age = 0;
+        color = "";
+    }
     // Write three more constructors for the Goat class: one that has just the name as an argument; one with name and age; and the last with all parameters.
-    Goat(string n)                  { name = n;}
-    Goat(string n, int goatAge)                  { name = n; age = goatAge;}
-    Goat(string n, int age, string color)        { name = n; age = age; color = "";}
+    Goat(string n) { name = n; }
+    Goat(string n, int goatAge)
+    {
+        name = n;
+        age = goatAge;
+    }
+    Goat(string n, int goatAge, string goatColor)
+    {
+        name = n;
+        age = goatAge;
+        color = goatColor;
+    }
     // setters and getters
-    void set_name(string n)         { name = n; };
-    string get_name() const         { return name; };
-    void set_age(int a)             { age = a; };
-    int get_age() const             { return age; }
-    void set_color(string c)        { color = c; }
-    string get_color() const        { return color; }
+    void set_name(string n) { name = n; };
+    string get_name() const { return name; };
+    void set_age(int a) { age = a; };
+    int get_age() const { return age; }
+    void set_color(string c) { color = c; }
+    string get_color() const { return color; }
 
     // write overloaded < operator for the std::list
 };
--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,8 @@
  *
  */
 
-// Write three more constructors for the Goat class: one that has just the name as an argument; one with name and age; and the last with all parameters.
-// Write a GM3K1 engine that loops on this menu. Write a main_menu() function that outputs this and then obtains, validates, and returns the user's choice.
-/**
-*** GOAT MANAGER 3001 ***
-[1] Add a goat
-[2] Delete a goat
-[3] List goats
-[4] Quit
-Choice --> 
- */
+// Done Write three more constructors for the Goat class: one that has just the name as an argument; one with name and age; and the last with all parameters.
+// Done Write a GM3K1 engine that loops on this menu. Write a main_menu() function that outputs this and then obtains, validates, and returns the user's choice.
 // Write functions to add a goat to the trip, delete a goat from the trip, and display the current trip.
 
 // When you're adding a goat, randomly select a name and color from main()'s arrays and select a random age between 0 and MAX_AGE.
@@ -50,6 +42,34 @@ void add_goat(list<Goat> &trip, string [], string []);
 void display_trip(list<Goat> trip);
 int main_menu();
 
+int main_menu() {
+    int choice;
+    cout << "*** GOAT MANAGER 3001 ***" << endl
+         << "[1] Add a goat"  << endl
+         << "[2] Delete a goat"  << endl
+         << "[3] List goats" << endl
+         << "[4] Quit" << endl
+         << "Choice --> "  << endl;
+    cin >> choice;
+    return choice;
+}
+
+int select_goat(list<Goat> trip) {
+    return 0;
+}
+void delete_goat(list<Goat> &trip) {
+
+}
+
+void add_goat(list<Goat> &trip, string [], string []) {
+// When you're adding a goat, randomly select a name and color from main()'s arrays and select a random age between 0 and MAX_AGE.
+
+}
+void display_trip(list<Goat> trip) {
+
+}
+
+
 int main() {
     srand(time(0));
     bool again;
@@ -65,6 +85,9 @@ int main() {
     i = 0;
     while (fin1 >> colors[i++]);
     fin1.close();
+
+    list<Goat> trip;
+    int choice = main_menu();
 
     return 0;
 }
